c0d008e2ca9117d5327574efeaff607e17c18478
Mohammed Al-said <mh884@users.noreply.github.com>
2022-04-10 10:49:45 -0400

    adding files

---
+++ src/list-Items.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"sync"
+
+	"github.com/1Password/connect-sdk-go/onepassword"
+)
+
+type onepasswordItems struct {
+	titles     []string
+	descs      []string
+	titleIndex int
+	descIndex  int
+	mtx        *sync.Mutex
+	shuffle    *sync.Once
+}
+
+func (r *onepasswordItems) reset(onepasswordItem []onepassword.Item) {
+	r.mtx = &sync.Mutex{}
+	r.shuffle = &sync.Once{}
+
+	size := len(onepasswordItem)
+	r.titles = make([]string, size)
+	r.descs = make([]string, size)
+	for k, v := range onepasswordItem {
+		r.titles[k] = v.Title
+		r.descs[k] = v.ID
+	}
+}
+
+func (r *onepasswordItems) next(onepasswordItem []onepassword.Item) item {
+	if r.mtx == nil {
+		r.reset(onepasswordItem)
+	}
+
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	i := item{
+		title:       r.titles[r.titleIndex],
+		description: r.descs[r.descIndex],
+	}
+
+	r.titleIndex++
+	if r.titleIndex >= len(r.titles) {
+		r.titleIndex = 0
+	}
+
+	r.descIndex++
+	if r.descIndex >= len(r.descs) {
+		r.descIndex = 0
+	}
+
+	return i
+}
